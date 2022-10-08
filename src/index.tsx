6bf4751a903f7fd4173d99cc43ec2532ad52abf5
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-10-08 21:03:27 +0200

    💄 Install and setup Material UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import CssBaseline from '@mui/material/CssBaseline'
+
 import { store } from './app/store'
 import App from './App'
-import './index.css'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Failed to find the root element!')
@@ -13,6 +19,7 @@ const root = createRoot(rootElement)
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
+			<CssBaseline />
 			<App />
 		</Provider>
 	</React.StrictMode>,
