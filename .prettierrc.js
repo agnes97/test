5ad72ff3f8a7cecd36da9103b2bcc691e60d8d93
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-10-08 16:22:52 +0200

    🎨 Install and apply Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/** @type {import('prettier').Config} */
+module.exports = {
+	arrowParens: 'avoid',
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	tabWidth: 2,
+}
