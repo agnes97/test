807a763dba9b7dd399a06153078a870ac0048fe7
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-10-08 22:00:24 +0200

    ♻️ Type functional components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Counter } from '../features/counter/Counter'
 
-export const Home = () => <Counter />
+export const Home: React.FC = () => <Counter />
 
 export default Home
