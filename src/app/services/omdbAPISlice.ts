946c061ed03cac9e17ef7ab880ca2182aa6e6399
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-10-11 08:09:31 +0200

    ♻️ Refactor ombdAPISlice types and query

--- src/app/services/omdbAPISlice.ts
+++ src/app/services/omdbAPISlice.ts
@@ -1,14 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-interface Movie {
+export interface Movie {
 	imdbID: string
 	Title: string
 	Year: string
 	Poster: string
 }
 
-interface MovieSearch {
+export interface MovieSearch {
 	Search: Movie[]
+	totalResults: string
 }
 
 const baseUrl = `http://www.omdbapi.com/`
@@ -18,16 +19,21 @@ export const omdbAPISlice = createApi({
 	reducerPath: 'omdbAPI',
 	baseQuery: fetchBaseQuery({ baseUrl }),
 	endpoints: builder => ({
-		search: builder.query<MovieSearch, string>({
-			query: input => ({
+		search: builder.query<
+			MovieSearch,
+			{ input: string; year: string; page: number }
+		>({
+			query: ({ input, year, page }) => ({
 				url: '',
 				params: {
 					apikey,
 					s: input,
+					year,
+					page,
 				},
 			}),
 		}),
 	}),
 })
 
-export const { useSearchQuery } = omdbAPISlice
+export const { useLazySearchQuery } = omdbAPISlice
