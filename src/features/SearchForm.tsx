41c2ce35b2fed377833f18fab56fb7aead68bc47
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-10-11 08:34:31 +0200

    ✨ Add SearchForm feature

---
+++ src/features/SearchForm.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+
+import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import SearchIcon from '@mui/icons-material/Search'
+
+interface SearchInput {
+	title: string
+	year: string
+}
+
+interface Props {
+	onSubmit: (input: SearchInput) => void
+}
+
+export const SearchForm: React.FC<Props> = ({ onSubmit }) => {
+	const [formData, setFormData] = useState<SearchInput>({
+		title: '',
+		year: '',
+	})
+
+	const handleInputChange = (
+		inputEvent: React.ChangeEvent<HTMLInputElement>,
+	): void => {
+		const { name, value } = inputEvent.target
+
+		setFormData(previousFormData => ({
+			...previousFormData,
+			[name]: value.toLocaleString(),
+		}))
+	}
+
+	return (
+		<Paper
+			id="search"
+			component="form"
+			onSubmit={event => {
+				event.preventDefault()
+
+				onSubmit(formData)
+			}}
+			variant="outlined"
+			sx={{
+				width: '100%',
+				display: 'flex',
+				flexDirection: {
+					xs: 'column',
+					md: 'row',
+				},
+				justifyContent: 'center',
+				gap: '1rem',
+				padding: '1rem',
+			}}
+		>
+			<TextField
+				name="title"
+				value={formData.title}
+				onChange={handleInputChange}
+				label="MOVIE TITLE"
+				placeholder="The Movie"
+				variant="filled"
+				sx={{ flexGrow: 1 }}
+			/>
+			<TextField
+				name="year"
+				value={formData.year}
+				onChange={handleInputChange}
+				label="YEAR"
+				placeholder="2022"
+				variant="filled"
+			/>
+			<Button
+				type="submit"
+				form="search"
+				variant="contained"
+				endIcon={<SearchIcon />}
+			>
+				SEARCH
+			</Button>
+		</Paper>
+	)
+}
